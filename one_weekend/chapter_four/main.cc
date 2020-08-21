707a4304d62a664f7d969b05fe830a9ed25847c1
BrianPopeck <bpopeck4@gmail.com>
2020-08-21 18:05:26 -0400

    finished chapter 4 and added check for successful compilation to render script

--- one_weekend/chapter_four/main.cc
+++ one_weekend/chapter_four/main.cc
@@ -1,19 +1,45 @@
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
 
+color ray_color(const ray& r) {
+    // lerp from blue to white as y direction decreases
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
 int main()
 {
-    const int image_width = 256;
-    const int image_height = 256;
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;    // distance from projection point to projection plane
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length); // of projection plane (-z is forwards in our coordinate system)
+
+    // Render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "Scanlines remaining: " << j << '\n';
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);  // ray at projection point in the direction of the projection plane
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
