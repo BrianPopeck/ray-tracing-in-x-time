b5b689c8aba58b404ce3d77b38a46415c2d77c1f
BrianPopeck <bpopeck4@gmail.com>
2020-08-23 22:58:03 -0400

    finished chapter 12

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ray-tracing-in-x-time
 Implementation of a ray tracer following Peter Shirley's book series here - https://raytracing.github.io/
+
+# Renders
+## one weekend
+![Many randomly placed spheres with a mixture of Lambertian, metal, and dielectric materials](one_weekend/chapter_thirteen/final_render.ppm)
---
+++ one_weekend/chapter_thirteen/color.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "vec3.h"
+
+#include <iostream>
+
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // scale color contribution down based on how many accumulated samples pixel_color represents
+    // "gamma 2" correction (raise to 1/2 power)
+    auto scale = 1.0 / samples_per_pixel;
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
+
+    // map color values to [0, 255]
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
+}
